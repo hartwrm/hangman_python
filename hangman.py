60b0443bf05a4078306bfc094adabbc1492b5958
Andrew Hartzell <achiv@themachine.local>
2022-02-15 23:36:30 -0500

    minor interface updates

--- hangman.py
+++ hangman.py
@@ -2,21 +2,49 @@ import random
 import requests
 import re
 
-# print(fetch_words.json())
+
+#Welcome to the game
+def welcome():
+    name = input("Enter your name\n")
+    print("Welcome" , name)
+    print("---------------")
+    print("Play hangman, you get 10 attempts to guess the word.")
+
+#play again?
+def play_again():
+    replay = input("Play again? Y/N?\n").lower()
+    if replay == 'y':
+        hangman()
+    else:
+        print("See you next time.")
+
+#run the game
 def hangman():
-    fetch_words = requests.get("https://random-word-api.herokuapp.com/word?number=10")
-    word = random.choice(fetch_words.json())
-    print(word)
+    #call welcom to start game play
+    welcome()
+
+    #fetch random word, print hint
+    word = requests.get("https://random-word-api.herokuapp.com/word?number=1").json()
+    print(word[0])
+    print("The word contains", len(word[0]), " letters")
+    print(len(word[0])*' _')
+
+    #set turns variable
     turns = 10
+
+    #already guess letters dictionary
+    used_letters = []
+
+    #get user input for word or letter
     search = input("guess a word or letter\n")
-    guess = re.findall(search, word)
+    
+    #use regex matching to search word
+    guess = re.findall(search, word[0])
     print(guess)
 
+    play_again()
 
-name = input("Enter your name\n")
 
-print("Welcome" , name)
-print("---------------")
-print("Play hangman, you get 10 attempts to guess the word")
+
 
 hangman()
